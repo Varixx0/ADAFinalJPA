697dc3d142f1f8adfa02e8a2a4a9c42b4ff265ad
Varixx0 <m.victortercer@gmail.com>
2024-01-09 14:53:59 +0100

    structure and menus

---
+++ GroupManager/src/main/java/com/groupmanager/GroupManager.java
@@ -0,0 +1,16 @@
+/*
+ * Esta es la clase que contiene el main de la aplicacion. 
+   Unicamente el punto de inicio, redirige a la clase controlador que se encarga del resto de cosas
+ */
+package com.groupmanager;
+
+/**
+ *
+ * @author victermug
+ */
+public class GroupManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
